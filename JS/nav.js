d01511ee03f8189d461ab0bcb3961f9db43b4698
alegaon <alegaon@outlook.com.ar>
2024-05-29 01:46:10 -0300

    se agrego logo cortina latiendo

--- JS/nav.js
+++ JS/nav.js
@@ -5,3 +5,15 @@ function openNav(){
 function closeNav(){
     document.getElementById('mobile-menu').style.width = "0%"
 }
+
+// cortina
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    setTimeout(() => {
+        document.querySelector('.cortina').style.opacity = '0';
+    }, 2000);
+
+    setTimeout(() => {
+        document.querySelector('.cortina').style.display = 'none';
+    }, 4000);
+});
